e76227e99f1049cc9ae9c3269f530c613cf86cd7
Sam Heyman <sam.heyman@gmail.com>
2019-04-06 14:48:06 +0200

    Add unit test placeholder

---
+++ test/unit_tests.dart
@@ -0,0 +1,11 @@
+// import 'package:flutter_test/flutter_test.dart';
+// import '../lib/model/bookings_model.dart';
+
+// test('adding a new gym class increases total number', () {
+//   final bookings = BookingsModel();
+//   final startingPrice = cart.totalPrice;
+//   cart.addListener(() {
+//     expect(cart.totalPrice, greaterThan(startingPrice));
+//   });
+//   cart.add(Item('Dash'));
+// });
