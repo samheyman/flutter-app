86778d5d02dfc674e2c8d79f3c8dc6ff8171e8e6
Sam Heyman <sam.heyman@gmail.com>
2019-04-08 14:03:10 +0200

    adding main.dart again, not sure why it keeps being removed

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+  import 'package:flutter/material.dart';
+  import 'package:scoped_model/scoped_model.dart';
+  import './bottom_navigation.dart';
+  import './pages/app_pages.dart';
+  import './model/bookings_model.dart';
+  import './utils/theme.dart';
+
+  void main() => runApp(
+    MamaFitClub(),
+  );
+
+  class MamaFitClub extends StatefulWidget {
+    @override
+    State<StatefulWidget> createState() => _MamaFitClubState();
+  }
+
+  class _MamaFitClubState extends State<MamaFitClub> {
+    int _currentIndex = 0;
+    final List<AppPage> _appPages = appPages;
+
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        title: 'Mama Fit Club',
+        home: Scaffold(
+          backgroundColor: Colors.red[50],
+          // appBar: AppBar(
+          //   title: Text((appPages[_currentIndex]).pageTitle),
+          // ),
+          body: _appPages[_currentIndex].pageWidget,
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            currentIndex: _currentIndex,
+            onTap: onTabTapped,
+            items: bottomNavigationItems,
+          ),
+        ),
+        theme: buildTheme(),
+      );
+    }
+
+    // Simple function to handle the bottom naviation  
+    void onTabTapped(int index) {
+      setState(() {
+        _currentIndex = index;
+      });
+    }
+  }
